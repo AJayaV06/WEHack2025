03f3fcafb08e46e0ad8b575a654d82d43557bee3
sreenidhi-gs <sreenidhigowri.sridhar@gmail.com>
2025-04-06 11:01:06 -0500

    Update Analytics.jsx

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Chatbot from './Chatbot';
+import Header from './Header.jsx';
 import './Analytics.css'; // Import the CSS for this component
 
 function Analytics() {
     return (
         <div className="analytics-container">
+            <Header></Header>
             <h1>Analytics</h1>
             <p>This is the analytics page. You can analyze your financial data and get insights here.</p>
             <div className="chatbot-wrapper">
